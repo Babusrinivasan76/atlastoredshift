48e751fb6a7ab7697dd3ab29c94bd2b52f898d36
Babu Srinivasan <101570105+Babusrinivasan76@users.noreply.github.com>
2022-08-21 12:55:50 +0530

    Create pyspark_atlastoredshift_customeractivity.py

---
+++ Scripts/pyspark_atlastoredshift_customeractivity.py
@@ -0,0 +1,32 @@
+# This script is used to migrate data from MongoDB Atlas to AWS Redshift using the Spark connectors
+###import the lib
+#import psycopg2
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+# create a local SparkSession
+spark = SparkSession.\
+        builder.\
+        appName("streamingExampleRead").\
+        config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.0').\
+        getOrCreate()
+
+#connection to the standar mongodb in atlas
+rdd=(spark.read.format("mongodb")
+        .option('spark.mongodb.connection.uri', 'mongodb+srv://s3load:s3load@s3load.frzascf.mongodb.net/?retryWrites=true&w=majority') \
+        .option('spark.mongodb.database', 'customer_activity') \
+        .option('spark.mongodb.collection', 'source') \
+.option('spark.mongodb.change.stream.publish.full.document.only','true') \
+        .option("forceDeleteTempCheckpointLocation", "true") \
+        .load())
+
+## write the dataframe to Redshift
+rdd.write.format('jdbc').options(
+        url='jdbc:redshift://partner-demo-redshift1.crzprzf2wp5s.us-east-1.redshift.amazonaws.com:5439/dev',
+        driver='com.amazon.redshift.jdbc42.Driver',
+        dbtable='public.customer_activity',
+        user='awsuser',
+        password='Password123').mode('append').save()
